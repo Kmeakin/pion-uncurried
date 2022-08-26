3caf6be6ce31df6239bc634c760ac78808dd6b62
Karl Meakin <karlwfmeakin@gmail.com>
2022-08-26 17:24:30 +0100

    add integration tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 #![warn(clippy::all, clippy::nursery, unused_qualifications)]
 #![allow(clippy::missing_const_for_fn, clippy::new_without_default)]
+#![feature(custom_test_frameworks)]
+#![test_runner(datatest::runner)]
 
 use std::rc::Rc;
 
 pub mod cli;
 pub mod core;
 pub mod surface;
+
+#[cfg(test)]
+mod tests;
 
 pub type RcStr = Rc<str>;
 pub type FileId = usize;
