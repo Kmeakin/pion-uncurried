a24250eb9dfb6e8dcf5fe4c2cd268241b95ff7c8
Karl Meakin <karlwfmeakin@gmail.com>
2022-10-05 02:09:27 +0100

    add `FileSpan`

--- crates/pion-salsa/src/ir/span.rs
+++ crates/pion-salsa/src/ir/span.rs
@@ -1,2 +1,10 @@
+use super::input_file::InputFile;
+
 pub type Span = text_size::TextRange;
 pub type Offset = text_size::TextSize;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+pub struct FileSpan {
+    pub file: InputFile,
+    pub span: Span,
+}
