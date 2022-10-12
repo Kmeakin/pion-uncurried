da5931a3bf80e4b050325f805178db84af232d33
Karl Meakin <karlwfmeakin@gmail.com>
2022-10-12 22:44:19 +0100

    delete experiments

--- pion-experiments/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![feature(once_cell)]
-#![warn(clippy::all, clippy::nursery, unused_qualifications)]
-#![allow(clippy::missing_const_for_fn, clippy::option_if_let_else)]
-
-pub mod env;
-pub mod semantics;
-pub mod surface;
-pub mod typecheck;
--- pion-experiments/src/surface.rs
+++
@@ -1,9 +0,0 @@
-use std::rc::Rc;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Expr<Span> {
-    Name(Span, String),
-    FunType(Span, String, Rc<Self>, Rc<Self>),
-    FunExpr(Span, String, Rc<Self>),
-    App(Span, Rc<Self>, Rc<Self>),
-}
